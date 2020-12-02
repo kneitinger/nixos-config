99eca93609d0f55131280408e466c4a07cbdd2ff
Kyle Kneitinger <kyle@kneit.in>
2020-12-02 01:30:26 -0800

    Add minimum.nix module for base configuration
    This module serves as the absolute bar minimum of tools, services, and
    configuration I prefer to have on a machine.

---
+++ modules/minimum.nix
@@ -0,0 +1,17 @@
+# Installs the bare minimum system utilities and settings I prefer
+
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    (neovim.override {withPython3 = true;})
+    git
+    htop
+    killall
+    ripgrep
+    python3
+    tmux
+    tree
+    wget
+  ];
+}
