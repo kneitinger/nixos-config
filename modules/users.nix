ffaade675b47960a1baebed569fec5ac73b5729e
Kyle Kneitinger <kyle@kneit.in>
2021-02-10 11:59:19 -0800

    Add user leaf to audio and dialout groups

--- modules/users.nix
+++ modules/users.nix
@@ -5,7 +5,7 @@
 {
   users.users.leaf = {
     isNormalUser = true;
-    extraGroups = [ "networkmanager" "video" "wheel" ];
+    extraGroups = [ "audio" "dialout" "networkmanager" "video" "wheel" ];
     shell = pkgs.zsh;
     # Allow login (locally) without password if user does not yet exist
     initialHashedPassword = "";
