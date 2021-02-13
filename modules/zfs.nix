1dc9a9a9ce527bcf6f4f12f0d52afcbaf2387e69
Kyle Kneitinger <kyle@kneit.in>
2021-02-13 01:25:48 -0800

    Make all zfs auto snap retentions explicit

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -11,8 +11,15 @@
     autoScrub.enable = true;
     autoSnapshot = {
       enable = true;
-      frequent = 8; # keep 2 hours of 15-min snapshots
-      monthly = 6;  # keep 6 monthly snapshots
+
+      # Note: these settings apply to all datasets with the property
+      # com.sun:auto-snapshot=true.  To o
+      frequent = 8; # every 15 mins
+      hourly = 24;
+      daily = 7;
+      weekly = 4;
+      monthly = 2;  # keep 6 monthly snapshots
+      flags = "--utc";
     };
     trim.enable = true;
   };
